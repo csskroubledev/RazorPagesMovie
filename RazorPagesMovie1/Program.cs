e938f3cf3db59e83277ded1760532171003891ca
csskrouble <csskrouble123@gmail.com>
2023-08-16 16:20:38 +0200

    add: project files

---
+++ RazorPagesMovie1/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie1.Data;
+using RazorPagesMovie1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovie1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovie1Context") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovie1Context' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
